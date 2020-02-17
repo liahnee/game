e15d94790c7f3e98c8321ae68f112842347a696e
Catherine Liahn Yang <liahnee@gmail.com>
2020-02-16 21:24:30 -0800

    hexagon map testing

--- src/component/Map.js
+++ src/component/Map.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import HexagonGrid from 'react-hexagon-grid';
+
+
+const hexagons = ['one', 'two', 3, 4, 'this']
 
 export default function Map() {
 
-    return null;
+    return (
+        <div> 
+            <HexagonGrid gridWidth={300} gridHeight={200} hexagons={hexagons}/>
+        </div>
+    );
 }
