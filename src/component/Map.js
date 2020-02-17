1d60f67abb090a81858f858485d5f7b62b2f1cea
Catherine Liahn Yang <liahnee@gmail.com>
2020-02-16 22:06:38 -0800

    new hex lib

--- src/component/Map.js
+++ src/component/Map.js
@@ -1,14 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import HexagonGrid from 'react-hexagon-grid';
+import '../stylesheets/map.css';
+import { HexGrid, Layout, Hexagon, GridGenerator, Text, Pattern, Path, Hex } from 'react-hexgrid';
 
-
-const hexagons = ['one', 'two', 3, 4, 'this']
-
+const num = [1, 2, 3, 4, 5];
 export default function Map() {
 
+
+
     return (
-        <div> 
-            <HexagonGrid gridWidth={300} gridHeight={200} hexagons={hexagons}/>
+        <div className='map'> 
+        <HexGrid width={'60vw'} height={'40vh'}>
+          <Layout size={{ x: 6, y: 6 }} spacing={1} origin={{x: 0, y: 0}}>
+            {num.map(n => {
+                return num.map( n2 => {
+                    return <Hexagon q={n} r={-n2} s={0} />
+                })
+            })}
+          </Layout>
+        </HexGrid>
         </div>
     );
 }
